ee091518f3327090c5d2df3bb796c8dd33c71292
abhishek <cashify@192.168.1.3>
2025-09-09 17:17:23 +0530

    fix: update API URL to production

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
   ? `https://me-api-playground-backend-9jod.onrender.com/projects/search/?query=${searchQuery}`
   : 'https://me-api-playground-backend-9jod.onrender.com/projects/';
     
-    fetch('https://me-api-playground-backend-9jod.onrender.com/projects/')
+    fetch('https://me-api-playground-backend-9jod.onrender.com/Projects/')
       .then(response => response.json())
       .then(data => setProjects(data))
       .catch(error => console.error('Error fetching projects:', error));
